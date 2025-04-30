934b4db9e16dac8db576a9d53b2c34fd19498c30
yungijin09 <s2546@e-mirim.hs.kr>
2025-04-30 16:47:27 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.java
+++ 프로그래머스/0/120829. 각도기/각도기.java
@@ -1,10 +1,14 @@
 class Solution {
     public int solution(int angle) {
         int answer = 0;
-        if (angle < 90) answer = 1;
-        if (angle == 90) answer = 2;
-        if (angle > 90) answer = 3;
-        if (angle == 180) answer = 4;
+        if (angle == 180){
+            answer = 4;
+        }else if (angle > 90){
+            answer = 3;
+        }else if (angle == 90){
+            answer = 2;
+        }else
+            answer = 1;
         
         return answer;
     }
